9dba473cdf1e7d1faa47f4e5bd2c06d8e79edaf7
Nicolas Noriega <40644310+NickN18@users.noreply.github.com>
2019-02-14 11:15:08 -0700

    Update selectMedication.php

--- selectMedication.php
+++ selectMedication.php
@@ -1,3 +1,15 @@
 <?php
+session_start();
+
+$conn = new mysqli($users, $userID, $userName, $userEmail);
+
+if(conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error); 
+}
+
+$sql = "SELECT userID, userName, userEmail"
+$result = $conn->query(sql);
+
+
 
 ?>
