7ee104a17c1045d2c85eefda70c9285e3599f625
Nicolas Noriega <40644310+NickN18@users.noreply.github.com>
2019-03-06 22:23:40 -0700

    Update medication-prescrip.php

--- medication-prescrip.php
+++ medication-prescrip.php
@@ -1,32 +1,18 @@
 <?php
 session_start();
-$activeMenu = "Medication";
-include "includes/header.php";
-include "includes/menu.php";
 
-$servername = getenv("IP");
-$username = getenv("C9_USER");
-$password = "";
-$database = "";
-$dbport = 3306;
- 
-$db = new mysqli($servername, $username, $password, $database, $dbport);
-
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
+if(isset($_SESSION['userName'])) {
+    header("Location: selectMedication.php");
+    
 }
 
+$activeMenu = "Medication";
+include("includes/header.php");
+include("includes/menu.php");
+
+include_once("includes/dbConnect.php");
+ 
 ?>
 <div id="content">
     This is the Medication page
 </div>
-<script>
-
-var selectMedication = document.querySelector('#selectMedication');
-selectMedication.addEventListener('click', selectMedicationClick, false);
-
-function selectMedicationClick() {
-    window.location = "selectMedication.php";
-}
-    
-</script>
